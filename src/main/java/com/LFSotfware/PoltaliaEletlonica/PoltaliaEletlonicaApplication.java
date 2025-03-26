42a577827c871770fdd15b58509b0c8f9feb79bf
LucianoSegundo <103046717+LucianoSegundo@users.noreply.github.com>
2025-03-26 19:03:46 -0300

    primeiro commit

---
+++ src/main/java/com/LFSotfware/PoltaliaEletlonica/PoltaliaEletlonicaApplication.java
@@ -0,0 +1,13 @@
+package com.LFSotfware.PoltaliaEletlonica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoltaliaEletlonicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoltaliaEletlonicaApplication.class, args);
+	}
+
+}
