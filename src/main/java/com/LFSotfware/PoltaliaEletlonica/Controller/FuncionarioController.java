dd311de6b1b7428a3533d9a4bbb7a83c37efeb29
LucianoSegundo <103046717+LucianoSegundo@users.noreply.github.com>
2025-03-28 17:39:54 -0300

    correção no metodo de atulização

--- src/main/java/com/LFSotfware/PoltaliaEletlonica/Controller/FuncionarioController.java
+++ src/main/java/com/LFSotfware/PoltaliaEletlonica/Controller/FuncionarioController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,7 +47,7 @@ public class FuncionarioController {
 		return ResponseEntity.ok().build();
 	}
 
-	@PostMapping(value = "/{idfunci}/definir/entrada")
+	@PutMapping (value = "/{idfunci}/definir/entrada")
 	public ResponseEntity<Void> definirEntrada(@PathVariable Long idfunci) {
 		try {
 			service.definirEntrada(idfunci);
@@ -62,7 +63,7 @@ public class FuncionarioController {
 		return ResponseEntity.ok().build();
 	}
 
-	@PostMapping(value = "/{idfunci}/definir/saida")
+	@PutMapping (value = "/{idfunci}/definir/saida")
 	public ResponseEntity<Void> definirSaida(@PathVariable Long idfunci) {
 
 		try {
